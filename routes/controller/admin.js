db2c24505ef64ad598be5bc79ea16e06c1dec433
njzwj <njzwj2007@126.com>
2016-11-25 20:14:44 +0800

    admin bugs fixed

--- routes/controller/admin.js
+++ routes/controller/admin.js
@@ -19,18 +19,27 @@ function showerr(err, object) {
     }
 }
 // universal page query
+// page
+// | pageNumber
+// | pageCount
+// | results
 var pageQuery = function(page, pageSize, Model, populate, queryParams, sortParams, callback) {
-  var start = (page - 1) * pageSize;
+  page = parseInt(page);
   var $page = {
     pageNumber: page
   };
-  async.parallel({
-    count: function(done) {
+  async.waterfall([
+    function(done) {
       Model.count(queryParams).exec(function (err, count) {
         done(err, count);
       });
     },
-    records: function(done) {
+    function(count, done) {
+      $page.pageCount = parseInt((count - 1) / pageSize) + 1;
+      if (page > $page.pageCount) page = $page.pageCount;
+      if (page < 1) page = 1;
+      $page.pageNumber = page;
+      var start = (page - 1) * pageSize;
       Model.find(queryParams)
         .skip(start)
         .limit(pageSize)
@@ -40,14 +49,13 @@ var pageQuery = function(page, pageSize, Model, populate, queryParams, sortParam
           done(err, doc);
         });
     }
-  }, function(err, results) {
-    var count = results.count;
-    $page.pageCount = (count - 1) / pageSize + 1;
-    $page.results = results.records;
+  ], function(err, doc) {
+    $page.results = doc;
     callback(err, $page);
   });
 }
 
+// login function isnot finished
 // login page
 router.get('/', function(req, res, next) {
   res.render('admin/login', { title: '管理员登录' });
@@ -61,12 +69,11 @@ router.post('/portal', function(req, res, next) {
 // news create update delete
 // req.body.mode = "create" | "update" | "delete"
 // req.body.content
-//   id, title, content, type, create_time, author
+// | id, title, content, type, create_time, author
 router.post('/newsdb', function(req, res, next) {
   var rdoc = req.body;
   if (rdoc.mode == 'delete') {
-    // console.log('deleting news no.' + rdoc._id);
-    nojdb.news.remove({ _id: rdoc._id }, showerr);
+    nojdb.news.remove({ _id: rdoc.id }, showerr);
   }else if (rdoc.mode == 'create') {
     var model = {
       title: rdoc.title,
@@ -82,18 +89,17 @@ router.post('/newsdb', function(req, res, next) {
       type: rdoc.type,
       author: "root"
     };
-    nojdb.news.update({ _id: rdoc._id }, model, showerr);
+    nojdb.news.update({ _id: rdoc.id }, model, showerr);
   }
   res.redirect("/admin/news");
 });
 
 // admin pages
 router.get('/news', function(req, res, next) {
-  // page
   var qdoc = req.query;
   var page = qdoc.page ? parseInt(qdoc.page) : 1;
-  // var pageSize = qdoc.pageSize ? parseInt(qdoc.pageSize) : 10;
-  pageQuery(page, 10, nojdb.news, '', '', { 'create_time': -1 }, function(err, results) {
+  var pageSize = 20;
+  pageQuery(page, pageSize, nojdb.news, '', '', { 'create_time': -1 }, function(err, results) {
     // set time format
     results.results.forEach(function (x) {
       x.create_time_string = moment(x.create_time).format(timeFormat);
@@ -109,34 +115,82 @@ router.get('/news', function(req, res, next) {
   });
 });
 router.get('/contest', function(req, res, next) {
-  res.render('admin/index', { title: '比赛管理', status: 'contest' });
+  var qdoc = req.query;
+  var page = qdoc.page ? parseInt(qdoc.page) : 1;
+  var pageSize = 20;
+  pageQuery(page, pageSize, nojdb.contest, '', '', { 'create_time': -1 }, function(err, results) {
+    // set time format
+    results.results.forEach(function (x) {
+      x.create_time_string = moment(x.begin_time).format(timeFormat);
+      x.create_time_string = moment(x.end_time).format(timeFormat);
+      x.create_time_string = moment(x.create_time).format(timeFormat);
+    });
+    res.render(
+      'admin/index',
+      { 
+        title: '比赛管理', 
+        status: 'contest',
+        page: results
+      }
+    );
+  });
 });
 router.get('/problem', function(req, res, next) {
-  res.render('admin/index', { title: '题目管理', status: 'problem' });
+  var qdoc = req.query;
+  var page = qdoc.page ? parseInt(qdoc.page) : 1;
+  var pageSize = 20;
+  pageQuery(page, pageSize, nojdb.question, '', '', { 'create_time': -1 }, function(err, results) {
+    // set time format
+    results.results.forEach(function (x) {
+      x.create_time_string = moment(x.create_time).format(timeFormat);
+    });
+    res.render(
+      'admin/index',
+      { 
+        title: '题目管理', 
+        status: 'problem',
+        page: results
+      }
+    );
+  });
+});
+
+// create pages
+router.get('/create/news', function(req, res, next) {
+  var context = {
+    _id: -1,
+    title: '创建公告',
+    mode: 'create',
+    model: {
+      title: '',
+      content: ''
+    }
+  };
+  res.render('admin/edit_news', context);
+});
+router.get('/create/problem', function(req, res, next) {
+  res.render('admin/edit_problem', { title: '创建题目'});
+});
+router.get('/create/contest', function(req, res, next) {
+  res.render('admin/edit_contest', { title: '创建比赛'});
 });
 
 // edit pages
 router.get('/edit/news', function(req, res, next) {
   var rdoc = req.query;
   var context = {
-    _id: -1,
-    mode: rdoc.mode,
+    _id: rdoc.id,
+    mode: 'update',
+    title: '编辑公告',
     model: {
       title: '',
       content: ''
     }
   };
-  if (rdoc.mode == 'create') {
-    context.title = '创建公告';
+  nojdb.news.findOne({ _id: context._id }, function(err, model) {
+    context.model = model;
     res.render('admin/edit_news', context);
-  }else if (rdoc.mode == 'update') {
-    context.title = '编辑公告';
-    context._id = rdoc._id;
-    nojdb.news.findOne({ _id: context._id }, function(err, model) {
-      context.model = model;
-      res.render('admin/edit_news', context);
-    });
-  }
+  });
 });
 router.get('/edit/contest', function(req, res, next) {
   res.render('admin/edit_contest', { title: '编辑比赛'});
